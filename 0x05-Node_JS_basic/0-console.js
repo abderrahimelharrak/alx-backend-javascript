9a54dc9e08fa570b12a525b2699597154baf4688
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2024-08-20 14:47:32 -0700

    task_0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Executing basic javascript with Node JS.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
